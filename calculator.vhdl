bace9ad558efa6d4f1ac397195a7f98a76cc03a7
rrsilaya <rrsilaya@up.edu.ph>
2018-05-12 12:48:32 +0800

    Create fetch decode constraints

--- calculator.vhdl
+++ calculator.vhdl
@@ -12,7 +12,8 @@ entity calculator is
     stage     : out STD_LOGIC_VECTOR(5 downto 0) := (others => '0');
     flag      : out STD_LOGIC_VECTOR(2 downto 0) := (others => '0');
     pc        : out STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
-    registers : out STD_LOGIC_VECTOR(31 downto 0) := (others => '0')
+    registers : out STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+    instruction : out STD_LOGIC_VECTOR(20 downto 0)
   );
 end entity;
 
@@ -21,7 +22,7 @@ architecture bev of calculator is
   type INSTRUCTIONS is array(14 downto 0) of STD_LOGIC_VECTOR(20 downto 0);
 
   -- Signals
-  signal instr_cache : INSTRUCTIONS;
+  --signal instr_cache : INSTRUCTIONS;
 
   begin
     process
@@ -30,7 +31,14 @@ architecture bev of calculator is
       VARIABLE buff      : LINE;
       VARIABLE ins       : INTEGER := 0;
       VARIABLE clk       : INTEGER := 0;
-      VARIABLE fetched   : INTEGER;
+
+      VARIABLE instr_cache : INSTRUCTIONS;
+      VARIABLE fetched   : STD_LOGIC_VECTOR(14 downto 0) := (others => '0');
+      VARIABLE decoded   : STD_LOGIC_VECTOR(14 downto 0) := (others => '0');
+      VARIABLE executed  : STD_LOGIC_VECTOR(14 downto 0) := (others => '0');
+
+      VARIABLE decoder   : INTEGER := 0;
+      VARIABLE progCt    : INTEGER := 0;
 
       CONSTANT FETCH     : INTEGER := 5;
       CONSTANT DECODE    : INTEGER := 4;
@@ -47,30 +55,46 @@ architecture bev of calculator is
           readline(content, buff);
           read(buff, opcode);
 
-          instr_cache(ins) <= opcode;
+          instr_cache(ins) := opcode;
 
           ins := ins + 1;
         end loop;
 
         file_close(content);
+
+        while ins /= 15 loop
+          instr_cache(ins) := std_logic_vector(to_unsigned(0, 21));
+          ins := ins + 1;
+        end loop;
       -- [/ File Reading ]
 
-      for clk in 0 to ins - 1 loop
+      for clk in 0 to 14 loop
         clock <= '1';
-        
-        -- Fetch
-        stage(FETCH) <= '1';
 
         -- Decode
+        if fetched(decoder) = '1' then
+          stage(DECODE) <= '1';
+          decoder := decoder + 1;
+        end if;
 
+        -- Fetch
+        if to_integer(unsigned(instr_cache(progCt)(2 downto 0))) /= 0 then
+          if to_integer(unsigned(instr_cache(progCt)(8 downto 3))) /= 0 then
+            stage(FETCH) <= '1';
+            fetched(progCt) := '1';
+          end if;
+        end if;
 
         -- Program Counter
-        pc <= std_logic_vector(to_unsigned((clk / 2) + 1, 4));
+        instruction <= instr_cache(progCt);
+        progCt := progCt + 1;
+        pc <= std_logic_vector(to_unsigned(progCt, 4));
 
         wait for 20 ns;
 
         -- Reset Skip
         clock <= '0';
+        instruction <= std_logic_vector(to_unsigned(0, 21));
         stage <= std_logic_vector(to_unsigned(0, 6));
         pc <= std_logic_vector(to_unsigned(0, 4));
         flag <= std_logic_vector(to_unsigned(0, 3));
--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@
 # 
 # NOTE: No need to include .vhdl extension.
 
-ghdl -a --ieee=synopsys $1.vhdl
-ghdl -e --ieee=synopsys $1
-ghdl -r --ieee=synopsys $1 --vcd=$1.vcd
+ghdl -a --ieee=synopsys $1.vhdl && \
+ghdl -e --ieee=synopsys $1 && \
+ghdl -r --ieee=synopsys $1 --vcd=$1.vcd && \
 gtkwave $1.vcd
